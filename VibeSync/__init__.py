61c2c99eb70df863290c00115dbe8616844f4483
AkashBatashCoder <itsakashbatash@gmail.com>
2026-03-20 11:23:36 +0600

    Build first prototype

---
+++ VibeSync/__init__.py
@@ -0,0 +1,49 @@
+import os 
+
+from flask import Flask, render_template
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+    
+    if test_config is None:
+        app.config.from_pyfile('config.py',silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    #ensure that the instance path exists
+    os.makedirs(app.instance_path, exist_ok=True)
+    
+    
+
+    # @app.route('/')
+    # def home():
+    #     return "This is the simple home page."
+
+    # @app.route('/hello')
+    # def hello():
+    #     return "Hello world! This is Hello page"
+
+    # Initialize the database
+    from . import db
+    db.init_app(app)
+
+    # Register the auth blueprint for auth  
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    # Register the blog blueprint for blog
+    from . import blog
+    app.register_blueprint(blog.bp)
+
+    app.add_url_rule('/', endpoint='index')
+
+
+    @app.errorhandler(404)
+    def not_found(e):
+        return render_template("404.html"), 404
+    return app
+    
